d1828dd09b59b4164e013ce8f1f9f7c2c2a0ae1b
Ashfaque Ahmed <155979001+AshfaqueAhmed123@users.noreply.github.com>
2024-05-01 13:53:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("search bar is not functional yet!! please bear.");
   }, 3000);
 });
+
+
+const form = document.querySelector(".search-form");
+const input = document.getElementById('search-input');
+form.addEventListner("submit",(e)=>{
+
+e.preventDefault();
+input.value="";
+  
+});
